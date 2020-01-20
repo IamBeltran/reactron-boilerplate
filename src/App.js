a3d9118d4e234de02be90a08a7c4a53a55d78b35
Victor Giovanni Beltrán Rodríguez <beltran-victor@outlook.com>
2020-01-19 21:04:00 -0600

    refact: restore App component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-// ▶ Import react dependecies
 import React from 'react';
-import { MemoryRouter as Router } from 'react-router-dom';
+import logo from './logo.svg';
+import './App.css';
 
-// ▶ Import components
-import Layout from './components/Layout';
-
-const App = () => {
+function App() {
   return (
-    <Router>
-      <Layout />
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
-};
+}
 
 export default App;
