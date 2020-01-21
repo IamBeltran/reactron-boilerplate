b4588cf277a12c59b74da0049013eeac0f6db02a
Victor Giovanni Beltrán Rodríguez <beltran-victor@outlook.com>
2020-01-20 20:39:23 -0600

    feat: add configurations

---
+++ system/configurations/index.js
@@ -0,0 +1,85 @@
+/* eslint-disable import/no-extraneous-dependencies */
+//  ┌───────────────────────────────────────────────────────────────────────────────────┐
+//  │ REQUIRE THIRDPARTY DEPENDENCIES MODULES.                                          │
+//  └───────────────────────────────────────────────────────────────────────────────────┘
+
+//  ┌───────────────────────────────────────────────────────────────────────────────────┐
+//  │ REQUIRE ELECTRON DEPENDENCIES MODULES.                                            │
+//  └───────────────────────────────────────────────────────────────────────────────────┘
+const electron = require('electron');
+
+//  ┌───────────────────────────────────────────────────────────────────────────────────┐
+//  │ REQUIRE NODEJS DEPENDENCIES MODULE.                                               │
+//  └───────────────────────────────────────────────────────────────────────────────────┘
+const path = require('path');
+
+//  ┌───────────────────────────────────────────────────────────────────────────────────┐
+//  │ REQUIRE MY DEPENDENCIES MODULES.                                                  │
+//  └───────────────────────────────────────────────────────────────────────────────────┘
+const helpers = require('../helpers');
+
+//  ┌───────────────────────────────────────────────────────────────────────────────────┐
+//  │ DESTRUCTURING DEPENDENCIES.                                                       │
+//  └───────────────────────────────────────────────────────────────────────────────────┘
+const { app } = electron;
+const { getAssets } = helpers;
+const { getIcons, getPackageJSON } = getAssets;
+
+//  ┌───────────────────────────────────────────────────────────────────────────────────┐
+//  │ DECLARATION OF CONSTANTS-VARIABLES.                                               │
+//  └───────────────────────────────────────────────────────────────────────────────────┘
+const packageJSONPath = getPackageJSON();
+const packageJSON = require(packageJSONPath);
+const hasVersion = !!packageJSON.version;
+const hasProductName = !!packageJSON.build.productName;
+const hasCopyright = !!packageJSON.build.copyright;
+
+const MY_APP_VERSION = '1.0.0';
+const MY_APP_NAME = 'Reactron Boilerplate';
+const MY_APP_COPYRIGHT = 'Copyright © 2014-2020 BRSoft, LLC';
+
+const APP_VERSION = hasVersion ? packageJSON.version : MY_APP_VERSION;
+const APP_NAME = hasProductName ? packageJSON.build.productName : MY_APP_NAME;
+const APP_TEAM = 'BRSoft, LLC';
+const APP_COPYRIGHT = hasCopyright ? packageJSON.build.copyright : MY_APP_COPYRIGHT;
+const USER_DATA_PATH = path.join(app.getPath('appData'), APP_NAME);
+const APP_ICON_FILE_X01 = getIcons('x01');
+const APP_ICON_FILE_X02 = getIcons('x02');
+const APP_ICON_FILE_X03 = getIcons('x03');
+const APP_ICON_FILE_X04 = getIcons('x04');
+const APP_ICON_FILE_X05 = getIcons('x05');
+const APP_ICON_FILE_X06 = getIcons('x06');
+const APP_ICON_FILE_X07 = getIcons('x07');
+const APP_ICON_FILE_X08 = getIcons('x08');
+const APP_ICON_FILE_X09 = getIcons('x09');
+const APP_ICON_FILE_X10 = getIcons('x10');
+const APP_ICON = getIcons('icon');
+
+//  ┌───────────────────────────────────────────────────────────────────────────────────┐
+//  │ DECLARATION OF AUXILIARY FUNCTIONS.                                               │
+//  └───────────────────────────────────────────────────────────────────────────────────┘
+
+//  ┌───────────────────────────────────────────────────────────────────────────────────┐
+//  │ SET MAIN MODULE - [NAME-MODULE].                                                  │
+//  └───────────────────────────────────────────────────────────────────────────────────┘
+const configurations = {
+  APP_VERSION,
+  APP_NAME,
+  APP_TEAM,
+  APP_COPYRIGHT,
+  USER_DATA_PATH,
+  APP_ICON_FILE_X01,
+  APP_ICON_FILE_X02,
+  APP_ICON_FILE_X03,
+  APP_ICON_FILE_X04,
+  APP_ICON_FILE_X05,
+  APP_ICON_FILE_X06,
+  APP_ICON_FILE_X07,
+  APP_ICON_FILE_X08,
+  APP_ICON_FILE_X09,
+  APP_ICON_FILE_X10,
+  APP_ICON,
+};
+
+//  ──[ EXPORT MODULE ]──────────────────────────────────────────────────────────────────
+module.exports = configurations;
