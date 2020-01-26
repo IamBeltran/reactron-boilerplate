ebad9cf7b0ad5a461ec093289b60c9f14b43091d
Victor Giovanni Beltrán Rodríguez <beltran-victor@outlook.com>
2020-01-26 16:09:45 -0600

    feat: add sortBy

--- src/components/ReadBooks/index.js
+++ src/components/ReadBooks/index.js
@@ -10,7 +10,7 @@ import { useQuery } from '@apollo/react-hooks';
 import './ReadBooks.css';
 
 const GET_BOOKS = gql`
-  {
+  query GetBooks {
     books {
       id
       title
@@ -19,22 +19,68 @@ const GET_BOOKS = gql`
   }
 `;
 
-const sortedBy = value => (a, b) => {
-  if (a[`${value}`] > b[`${value}`]) {
-    return 1;
-  }
-  if (b[`${value}`] > a[`${value}`]) {
-    return -1;
+const sortBy = ({ key, asc }) => (first, second) => {
+  if (asc) {
+    if (first[`${key}`] > second[`${key}`]) return 1;
+    if (second[`${key}`] > first[`${key}`]) return -1;
+  } else {
+    if (first[`${key}`] > second[`${key}`]) return -1;
+    if (second[`${key}`] > first[`${key}`]) return 1;
   }
   return 0;
 };
 
+const Loading = ({ onClosePortal02 }) => {
+  return (
+    <div id="read-books">
+      <div id="modal-control">
+        <button type="button" className="btn-modal" onClick={onClosePortal02}>
+          X
+        </button>
+      </div>
+      <div id="main-wrapper">
+        <p>Loading...</p>
+      </div>
+    </div>
+  );
+};
+
+Loading.propTypes = {
+  onClosePortal02: PropTypes.func.isRequired,
+};
+
+const QueryError = ({ error, onClosePortal02 }) => {
+  return (
+    <div id="read-books">
+      <div id="modal-control">
+        <button type="button" className="btn-modal" onClick={onClosePortal02}>
+          X
+        </button>
+      </div>
+      <div id="main-wrapper">
+        <p>{error}</p>
+      </div>
+    </div>
+  );
+};
+
+QueryError.propTypes = {
+  error: PropTypes.func.isRequired,
+  onClosePortal02: PropTypes.func.isRequired,
+};
+
 const ReadBooks = props => {
   const { onClosePortal02 } = props;
-  const { loading, error, data } = useQuery(GET_BOOKS);
+  const { data, loading, error } = useQuery(GET_BOOKS, {
+    variables: {
+      cursor: null,
+    },
+    fetchPolicy: 'cache-and-network',
+  });
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>{`Error: ${error}`}</p>;
+  if (loading) return <Loading onClosePortal02={onClosePortal02} />;
+  if (error) return <QueryError error={error} onClosePortal02={onClosePortal02} />;
+  // const { books } = data;  type: $type, offset: $offset, limit: $limit
   return (
     <div id="read-books">
       <div id="modal-control">
@@ -53,7 +99,7 @@ const ReadBooks = props => {
               </tr>
             </thead>
             <tbody>
-              {data.books.sort(sortedBy('title')).map(({ id, title, author }) => (
+              {data.books.sort(sortBy({ key: 'title', asc: true })).map(({ id, title, author }) => (
                 <tr key={id}>
                   <td>{title}</td>
                   <td>{author}</td>
