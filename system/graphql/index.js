7ffba3b2eb61278c39947df6db2b98f99afc3fc8
Victor Giovanni Beltrán Rodríguez <beltran-victor@outlook.com>
2020-01-30 22:46:29 -0600

    refact: rename module

--- system/graphql/index.js
+++ system/graphql/index.js
@@ -5,8 +5,8 @@ const schema = require('./schema');
 const rootValue = require('./rootValue');
 
 //  ──[ EXPORT MODULE ]──────────────────────────────────────────────────────────────────
-const graphQLUtils = (module.exports = exports = {}); // eslint-disable-line no-multi-assign
+const graphQL = (module.exports = exports = {}); // eslint-disable-line no-multi-assign
 
 // » Main Modules
-graphQLUtils.schema = schema;
-graphQLUtils.rootValue = rootValue;
+graphQL.schema = schema;
+graphQL.rootValue = rootValue;
